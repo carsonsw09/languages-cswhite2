8b1f0a40e936c40495fdb37d1458c28598268d53
carsonsw09 <carsonsw09@gmail.com>
2024-10-10 17:42:16 +0000

    Functional programming assignment is updated and finished

--- Assignments/Functional Programming/build.sbt
+++ Assignments/Functional Programming/build.sbt
@@ -1,13 +1,13 @@
-// build.sbt
 ThisBuild / scalaVersion := "2.13.12"
 
-lazy val root = (project in file("."))
-  .settings(
-    name := "HelloWorldScalaApp",
-    version := "0.1",
-    libraryDependencies ++= Seq(
-      "com.typesafe.akka" %% "akka-actor-typed" % "[2.6,3.0)",  // Allows any version from 2.6.x up to (but not including) 3.0
-      "com.typesafe.akka" %% "akka-http"        % "[10.2,11.0)", // Allows any version from 10.2.x up to (but not including) 11.0
-      "com.typesafe.akka" %% "akka-stream"      % "[2.6,3.0)"   // Allows any version from 2.6.x up to (but not including) 3.0
-    )
-  )
+// Include other dependencies as needed
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor-typed" % "2.6.19",
+  "com.typesafe.akka" %% "akka-http" % "10.2.9",
+  "com.typesafe.akka" %% "akka-stream" % "2.6.19",
+  "com.typesafe.akka" %% "akka-http-spray-json" % "10.2.10"//Akka HTTP JSON support
+)
+
+// Specify the main class if sbt is not detecting it
+mainClass in Compile := Some("HelloWorldServer")
+
--- Assignments/Functional Programming/src/main/scala/HelloWorldServer.scala
+++ Assignments/Functional Programming/src/main/scala/HelloWorldServer.scala
@@ -1,20 +1,28 @@
-// src/main/scala/HelloWorldServer.scala
-
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
-
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json._
 import scala.io.StdIn
+import scala.io.Source
 
-object HelloWorldServer {
+// Case class to represent the list of strings in JSON
+final case class StringList(strings: List[String])
+
+// JSON format for the case class
+trait JsonSupport extends DefaultJsonProtocol {
+  implicit val stringListFormat = jsonFormat1(StringList)
+}
+
+object HelloWorldServer extends JsonSupport {
 
   def main(args: Array[String]): Unit = {
     // Create an ActorSystem
     implicit val system = ActorSystem(Behaviors.empty, "helloWorldSystem")
     implicit val executionContext = system.executionContext
 
-    // Define the route
+    // Define the routes
     val route =
       path("greet" / Segment) { person =>
         get {
@@ -25,10 +33,19 @@ object HelloWorldServer {
         get {
           complete(s"Hello!")
         }
+      } ~
+      path("sortStringsFromFile") {
+        get {
+          // Use the correct path to the JSON file
+          val jsonFile = Source.fromFile("src/main/scala/strings.json").getLines.mkString
+          val stringList = jsonFile.parseJson.convertTo[StringList] // Parse JSON into StringList
+          val sortedStrings = stringList.strings.sorted             // Sort the list of strings
+          complete(StringList(sortedStrings))                       // Return the sorted list as JSON
+        }
       }
 
     // Start the server
-    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+    val bindingFuture = Http().newServerAt("0.0.0.0", 8080).bind(route)
 
     println("Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // Keep the server running until user presses return
